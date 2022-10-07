a15d23871462dd142b3ffab87730cc2fa200b5dc
Lukasz <skotniczny@outlook.com>
2022-10-07 23:03:31 +0200

    Zamiana parametrów

---
+++ ZamianaParametrów/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ZamianaParametrów
+{
+    internal class Program
+    {
+        static void swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            Console.WriteLine($"a={a} b={b}");
+            swap(ref a, ref b);
+            Console.WriteLine($"a={a} b={b}");
+        }
+    }
+}
