d04929638580f7051c6dca404c08106131e287e4
Lukasz <skotniczny@outlook.com>
2022-10-07 22:53:22 +0200

    Parzystość

---
+++ Parzystość/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Parzystość
+{
+    internal class Program
+    {
+        static bool czyLiczbaJestParztysta(int a)
+        {
+            return a % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"0: {czyLiczbaJestParztysta(0)}");
+            Console.WriteLine($"1: {czyLiczbaJestParztysta(1)}");
+            Console.WriteLine($"2: {czyLiczbaJestParztysta(2)}");
+            Console.WriteLine($"3: {czyLiczbaJestParztysta(3)}");
+            Console.WriteLine($"4: {czyLiczbaJestParztysta(4)}");
+            Console.WriteLine($"5: {czyLiczbaJestParztysta(5)}");
+        }
+    }
+}
