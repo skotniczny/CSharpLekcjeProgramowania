1c3e8f78fc795adf4245c4cb6fab427804dc2cf8
Lukasz <skotniczny@outlook.com>
2022-10-07 22:39:54 +0200

    Łańcuchy

---
+++ Łańcuchy/Program.cs
@@ -0,0 +1,8 @@
+﻿string s = "Hello";
+s += " World!";
+Console.WriteLine("Łańcuch: " + s);
+Console.WriteLine("Długość: " + s.Length);
+Console.WriteLine("Drukowanymi: " + s.ToUpper());
+string r = s.ToUpper().Replace('L', '1').Replace('O', '0').Replace('E', '3');
+Console.WriteLine("Zmodyfikowany łańcuch: " + r);
+char[] litery = s.ToCharArray();
