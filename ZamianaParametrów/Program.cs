133c83033f023171c50579ac1d2864e69e17e10a
Lukasz <skotniczny@outlook.com>
2022-10-07 23:03:31 +0200

    Metoda ogólna (R04, Metody. Ćwiczenia)

--- ZamianaParametrów/Program.cs
+++ ZamianaParametrów/Program.cs
@@ -2,9 +2,9 @@
 {
     internal class Program
     {
-        static void swap(ref int a, ref int b)
+        static void swap<T>(ref T a, ref T b)
         {
-            int tmp = a;
+            T tmp = a;
             a = b;
             b = tmp;
         }
@@ -13,8 +13,18 @@
         {
             int a = 1, b = 2;
             Console.WriteLine($"a={a} b={b}");
-            swap(ref a, ref b);
+            swap<int>(ref a, ref b);
             Console.WriteLine($"a={a} b={b}");
+             
+            double c = 1, d = 2;
+            Console.WriteLine($"c={c} d={d}");
+            swap<double>(ref c, ref d);
+            Console.WriteLine($"c={c} d={d}");
+
+            string e = "jeden", f = "dwa";
+            Console.WriteLine($"e={e} f={f}");
+            swap(ref e, ref f);
+            Console.WriteLine($"e={e} f={f}");
         }
     }
 }
