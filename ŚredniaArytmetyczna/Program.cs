c0ac253c727feb71fce8fbee162a3af36fbbcd2f
Lukasz <skotniczny@outlook.com>
2022-10-07 22:08:49 +0200

    Średnia arytmetyczna

---
+++ ŚredniaArytmetyczna/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ŚredniaArytmetyczna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a = ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("b = ");
+                int b = int.Parse(Console.ReadLine());
+                double średnia = (a + b) / 2.0;
+                Console.WriteLine($"Średnia liczb {a} i {b} to {średnia}");
+            }
+            catch (Exception exc)
+            {
+                Console.Error.WriteLine("Wystąpił błąd: " + exc.Message);
+            }
+        }
+    }
+}
