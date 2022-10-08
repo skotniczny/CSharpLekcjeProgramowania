5589fac2f5344d7a3aa312705eb976e38d35af0b
Łukasz <skotniczny@outlook.com>
2022-10-08 21:35:53 +0200

    Zabawa dwiema kostkami

---
+++ ZabawaKostkami/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] zbiórSumOczekZDwóchKostek(int liczbaRzutówKostką = 100)
+{
+    Random r = new Random();
+    int[] wyniki = new int[liczbaRzutówKostką];
+    for (int i = 0; i < wyniki.Length; ++i)
+    {
+        int pierwszaKostka = r.Next(1, 7);
+        int drugaKostka = r.Next(1, 7);
+        wyniki[i] = pierwszaKostka + drugaKostka;
+    }
+    return wyniki;
+}
+
+int[] wartości = zbiórSumOczekZDwóchKostek(100);
+foreach (int wartość in wartości) Console.Write($"{wartość}; ");
