dcf842e224922790aca6a623f10fdef02bbdc338
Łukasz <skotniczny@outlook.com>
2022-10-08 21:51:27 +0200

    Agregacja danych

--- ZabawaKostkami/Program.cs
+++ ZabawaKostkami/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] zbiórSumOczekZDwóchKostek(int liczbaRzutówKostką = 100)
+﻿int[] ZbiórSumOczekZDwóchKostek(int liczbaRzutówKostką = 100)
 {
     Random r = new Random();
     int[] wyniki = new int[liczbaRzutówKostką];
@@ -11,5 +11,43 @@
     return wyniki;
 }
 
-int[] wartości = zbiórSumOczekZDwóchKostek(100);
+double Suma(double[] wartości)
+{
+    double suma = 0;
+    foreach (double wartość in wartości) suma += wartość;
+    return suma;
+}
+
+double Średnia(double[] wartości)
+{
+    if (wartości == null) throw new ArgumentNullException("Przesłano obiekt pusty");
+    if (wartości.Length == 0) throw new ArgumentException("W tablicy nie ma elementów");
+    return Suma(wartości) / wartości.Length;
+}
+
+double Wariancja(double[] wartości)
+{
+    double średnia = Średnia(wartości);
+    double wariancja = 0;
+    foreach (double wartość in wartości)
+    {
+        double odchylenie = wartość - średnia;
+        wariancja += odchylenie * odchylenie;
+    }
+    return wariancja / wartości.Length;
+}
+
+double OdchylenieStandardowe(double[] wartości)
+{
+    return Math.Sqrt(Wariancja(wartości));
+}
+
+int[] wartości = ZbiórSumOczekZDwóchKostek(100);
 foreach (int wartość in wartości) Console.Write($"{wartość}; ");
+double[] tablica = Array.ConvertAll<int, double>(wartości, i => (double)i);
+Console.WriteLine($"Liczba elementów: {tablica.Length}");
+Console.WriteLine($"Suma: {Suma(tablica)}");
+Console.WriteLine($"Średnia: {Średnia(tablica)}");
+Console.WriteLine($"Wariancja: {Wariancja(tablica)}");
+Console.WriteLine($"Odchylenie standardowe: {OdchylenieStandardowe(tablica)}");
+
