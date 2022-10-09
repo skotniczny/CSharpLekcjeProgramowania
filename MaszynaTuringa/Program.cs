4940cdade7e9841f340a402ce5457c7ac1d1325e
Lukasz <skotniczny@outlook.com>
2022-10-09 17:04:28 +0200

    Dodawanie plików tekstowych do projektu

---
+++ MaszynaTuringa/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MaszynaTuringa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kodProgramu = File.ReadAllLines("program.txt");
+            string łańcuchOpisującyStanMaszyny = File.ReadAllText("taśma.txt");
+            Console.WriteLine($"Początkowy stan maszyny: {łańcuchOpisującyStanMaszyny}");
+            Console.WriteLine($"Program:");
+            foreach (string linia in kodProgramu) Console.WriteLine(linia);
+        }
+    }
+}
