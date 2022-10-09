46bdaa1451d2b15d120ecf47b676ebeef6b022e7
Lukasz <skotniczny@outlook.com>
2022-10-09 15:53:00 +0200

    Debugowanie

---
+++ Debugowanie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Debugowanie
+{
+    internal class Program
+    {
+        static private int kwadrat(int argument)
+        {
+            int wartość;
+            wartość = checked(argument * argument);
+            if (wartość < 0)
+            {
+                throw new Exception("Funkcja kwadrat nie powinna dwracać wartości ujemnej!");
+            }
+            return wartość;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 1234;
+                int y = kwadrat(x);
+                y = kwadrat(y);
+                string sy = y.ToString();
+                Console.WriteLine(sy);
+            }
+            catch(Exception ex)
+            {
+                ConsoleColor bieżącyKolor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine($"Błąd: {ex.Message}");
+                Console.ForegroundColor = bieżącyKolor;
+            }
+            
+        }
+    }
+}
