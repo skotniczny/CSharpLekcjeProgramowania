5278e5879649e8aa1a942e810155113f307734a8
Lukasz <skotniczny@outlook.com>
2022-10-09 22:22:04 +0200

    Argumenty linii komend

--- MaszynaTuringa/Program.cs
+++ MaszynaTuringa/Program.cs
@@ -99,8 +99,30 @@
 
         static void Main(string[] args)
         {
-            string[] kodProgramu = File.ReadAllLines("program.txt");
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Do uruchomienia program wymaga dwóch argumentów");
+                return;
+            }
+
+            string ścieżkaPlikuProgramu = args[0];
+            if (!File.Exists(ścieżkaPlikuProgramu))
+            {
+                Console.Error.WriteLine($"Brak pliku z kodem programu {ścieżkaPlikuProgramu}");
+                return;
+            }
+            Console.WriteLine($"Pliki z kodem programu {ścieżkaPlikuProgramu}");
+            string[] kodProgramu = File.ReadAllLines(ścieżkaPlikuProgramu);
+
+            string ścieżkaPlikuTaśmy = args[1];
+            if (!File.Exists(ścieżkaPlikuTaśmy))
+            {
+                Console.Error.WriteLine($"Brak pliku ze stanem maszyny {ścieżkaPlikuTaśmy}");
+                return;
+            }
+            Console.WriteLine($"Plik ze stanem maszyny {ścieżkaPlikuTaśmy}");
             string łańcuchOpisującyStanMaszyny = File.ReadAllText("taśma.txt");
+
             Console.WriteLine($"Początkowy stan maszyny: {łańcuchOpisującyStanMaszyny}");
             Console.WriteLine($"Program:");
             foreach (string linia in kodProgramu) Console.WriteLine(linia);
