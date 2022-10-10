30510137fe08bc42ec5413184187183cf9d4a990
Lukasz <skotniczny@outlook.com>
2022-10-10 13:48:12 +0200

    Pierwsze testy

--- UłamekDemo/Program.cs
+++ UłamekDemo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Ułamek.Info());
+
+            Ułamek u13 = new Ułamek(1, 3);
+            Ułamek u2 = new Ułamek(2);
+            Ułamek u0 = Ułamek.Zero;
+            Ułamek uP = Ułamek.Połowa;
+
+            Console.WriteLine(u13.ToString());
+            Console.WriteLine(u2.ToString());
+            Console.WriteLine(u0.ToString());
+            Console.WriteLine(uP.ToString());
         }
     }
 }
