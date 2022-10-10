9a79431afd9b3a783b80634730c83a6b17a98106
Lukasz <skotniczny@outlook.com>
2022-10-10 13:39:38 +0200

    Konstruktor i statyczne pola

--- UłamekDemo/Ułamek.cs
+++ UłamekDemo/Ułamek.cs
@@ -9,5 +9,25 @@ namespace UłamekDemo
     public struct Ułamek
     {
         private int licznik, mianownik;
+
+        public Ułamek(int licznik, int mianiownik = 1)
+        {
+            if (mianiownik == 0)
+            {
+                throw new ArgumentException("Mianiwnik musi być różny od zera");
+            }
+            this.licznik = licznik;
+            this.mianownik = mianiownik;
+        }
+
+        public static readonly Ułamek Zero = new Ułamek(0);
+        public static readonly Ułamek Jeden = new Ułamek(1);
+        public static readonly Ułamek Połowa = new Ułamek(1, 2);
+        public static readonly Ułamek Ćwierć = new Ułamek(1, 4);
+
+        public static string Info()
+        {
+            return "Struktura Ułamek, (c) Jacek Matulewki 2020";
+        }
     }
 }
