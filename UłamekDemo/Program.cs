7d0c6c60601c938a06115799e94e03d06665abfc
Lukasz <skotniczny@outlook.com>
2022-10-10 13:38:52 +0200

    Przygotowanie projektu

---
+++ UłamekDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UłamekDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ UłamekDemo/Ułamek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UłamekDemo
+{
+    public struct Ułamek
+    {
+        private int licznik, mianownik;
+    }
+}
