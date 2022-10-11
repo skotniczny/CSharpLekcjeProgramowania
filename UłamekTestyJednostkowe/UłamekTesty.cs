638e35bf0404978b91f808759271474ab622f715
Łukasz <skotniczny@outlook.com>
2022-10-11 20:24:19 +0200

    Pierwszy test jednostkowy

--- UłamekTestyJednostkowe/UłamekTesty.cs
+++ UłamekTestyJednostkowe/UłamekTesty.cs
@@ -6,8 +6,18 @@ namespace UłamekTestyJednostkowe
     public class UłamekTesty
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestKonstruktoraIWłasności()
         {
+            //przygotowania (arrange)
+            int licznik = 1;
+            int mianownik = 2;
+
+            //działanie (act)
+            Ułamek u = new Ułamek(licznik, mianownik);
+
+            //werfikacja (assert)
+            Assert.AreEqual(licznik, u.Licznik, "Niezgodność w liczniku");
+            Assert.AreEqual(mianownik, u.Mianownik, "Niezgodność w mianowniku");
         }
     }
 }
