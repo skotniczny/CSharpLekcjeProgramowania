359de1cd3d4e84e08aedebf5ef15c57fa9dab9cf
Łukasz <skotniczny@outlook.com>
2022-10-12 22:04:19 +0200

    Obliczanie sumy szeregu

---
+++ Z0508/Program.cs
@@ -0,0 +1,13 @@
+﻿static double sumaSzeregu(Func<long, double> f, long n)
+{
+    double sum = 0;
+    for (long i = 1; i < n; i++)
+    {
+        sum += f(i);
+    }
+    return sum;
+}
+
+double sum = sumaSzeregu((x) => 1.0 / (x * x), 1000000);
+Console.WriteLine($"PI = {Math.Sqrt(sum * 6)}");
+Console.WriteLine($"Błąd = {Math.PI - Math.Sqrt(sum * 6)}");
