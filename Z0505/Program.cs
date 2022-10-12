6cc3ffbde5c8574cff6869cf849350c414d8a06c
Łukasz <skotniczny@outlook.com>
2022-10-12 21:15:54 +0200

    Kwadrat wpisanej liczby

---
+++ Z0505/Program.cs
@@ -0,0 +1,15 @@
+﻿bool success;
+do
+{
+    Console.Write("Napisz liczbę z zakresu 1-10:");
+    success = int.TryParse(Console.ReadLine(), out int liczba);
+    if (liczba < 1 || liczba > 10)
+    {
+        Console.WriteLine("Podano wartość spoza zakresu!");
+        success = false;
+    } else
+    {
+        Console.WriteLine($"Kwadrat wpisanej liczby: {liczba * liczba}");
+    }
+}
+while (!success);
