c3b928f047736f069c9e6916fe6c4c3b898a898f
Łukasz <skotniczny@outlook.com>
2022-10-12 21:54:29 +0200

    Implementacja metody Całka

---
+++ Z0507/Program.cs
@@ -0,0 +1,13 @@
+﻿static double Całka(Func<double, double> f, double a, double b, uint N)
+{
+    double h = (b - a) / N;
+    double sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += f(a + h / 2 + i * h);
+    }
+    return h * sum;
+}
+
+double wynik = Całka(x => x * x, 0, 1, 1000);
+Console.WriteLine("Wynik: " + wynik);
