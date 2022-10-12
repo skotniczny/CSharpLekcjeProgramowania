0eeb2a1aa8a8db4c38b9c5246c28f2ee25405b0c
Łukasz <skotniczny@outlook.com>
2022-10-12 20:51:32 +0200

    NWD i NWW

---
+++ Z0504/Program.cs
@@ -0,0 +1,23 @@
+﻿int Nwd(int a, int b)
+{
+    int c;
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+int Nww(int a, int b)
+{
+    return a * b / Nwd(a, b);
+}
+
+int a = 45;
+int b = 195;
+
+Console.WriteLine($"a={a} b={b}");
+Console.WriteLine($"NWD: {Nwd(a, b)}");
+Console.WriteLine($"NWW: {Nww(a, b)}");
