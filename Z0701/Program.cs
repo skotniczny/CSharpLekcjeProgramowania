960a5a5615b9ba10657e583e3f89bb0e6284c3e8
Łukasz <skotniczny@outlook.com>
2022-10-13 10:35:23 +0200

    Potęgi liczby 2

---
+++ Z0701/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] potęgi2 = new int[10];
+int podstawa = 2;
+int indeks = podstawa - podstawa;
+int potęga = podstawa / podstawa;
+for (int i = indeks; i < potęgi2.Length; i++)
+{
+    potęgi2[i] = potęga *= podstawa;
+}
+
+Console.WriteLine(String.Join(',', potęgi2));
