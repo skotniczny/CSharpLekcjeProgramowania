af25445016033e128e8eab7346d0967b5e2b359e
Łukasz <skotniczny@outlook.com>
2022-10-13 20:58:22 +0200

    Dwójka

--- MaszynaTuringa2/Turing.cs
+++ MaszynaTuringa2/Turing.cs
@@ -1,5 +1,18 @@
 ﻿namespace MaszynaTuringa2
 {
+    struct Dwójka : IComparable<Dwójka>
+    {
+        public char StanGłowicy;
+        public char WartośćLubPolecenieNaTaśmie;
+
+        public int CompareTo(Dwójka other)
+        {
+            int wartosc = StanGłowicy.CompareTo(other.StanGłowicy);
+            if (wartosc != 0) return wartosc;
+            else return WartośćLubPolecenieNaTaśmie.CompareTo(other.WartośćLubPolecenieNaTaśmie);
+        }
+    }
+
     class Turing
     {
     }
