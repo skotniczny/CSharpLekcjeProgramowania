f6be712ef28adb8cc422101b9727434fb61d199a
Łukasz <skotniczny@outlook.com>
2022-10-13 10:55:06 +0200

    Wybór elementów z tablicy

---
+++ Z0702/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] ti = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine($"ti[2]: {ti[2]}");
+// Console.WriteLine($"ti[10] {ti[10]}");
+Console.WriteLine($"ti[4..8] {string.Join(',', ti[4..8])}");
