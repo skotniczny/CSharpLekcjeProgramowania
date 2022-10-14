0d62e02c355386da5ff10af6ec71c92d15af73b9
Łukasz <skotniczny@outlook.com>
2022-10-14 08:33:13 +0200

    Rozruch

--- MaszynaTuringa2/Program.cs
+++ MaszynaTuringa2/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace MaszynaTuringa2
+﻿using Turing;
+
+namespace MaszynaTuringa2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Do uruchomienia program wymaga dwóch argumentów");
+                return;
+            }
+
+            string ścieżkaPlikuProgramu = args[0];
+            if (!File.Exists(ścieżkaPlikuProgramu))
+            {
+                Console.Error.WriteLine($"Brak pliku z kodem programu {ścieżkaPlikuProgramu}");
+                return;
+            }
+            Console.WriteLine($"Pliki z kodem programu {ścieżkaPlikuProgramu}");
+            string[] kodProgramu = File.ReadAllLines(ścieżkaPlikuProgramu);
+
+            string ścieżkaPlikuTaśmy = args[1];
+            if (!File.Exists(ścieżkaPlikuTaśmy))
+            {
+                Console.Error.WriteLine($"Brak pliku ze stanem maszyny {ścieżkaPlikuTaśmy}");
+                return;
+            }
+            Console.WriteLine($"Plik ze stanem maszyny {ścieżkaPlikuTaśmy}");
+            string łańcuchOpisującyStanMaszyny = File.ReadAllText("taśma.txt");
+
+            Console.WriteLine($"Początkowy stan maszyny: {łańcuchOpisującyStanMaszyny}");
+            Console.WriteLine($"Program:");
+            foreach (string linia in kodProgramu) Console.WriteLine(linia);
+
+            try
+            {
+                Turing.MaszynaTuringa maszyna = new Turing.MaszynaTuringa(łańcuchOpisującyStanMaszyny, kodProgramu);
+                string[] historia = maszyna.WykonajProgram();
+
+                Console.WriteLine("\nEwolucja stanu maszyny:");
+                foreach (string linia in historia)
+                {
+                    Console.WriteLine(linia);
+                }
+            }
+            catch (Exception exc)
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine($"Błąd: {exc.Message}");
+                Console.ForegroundColor = color;
+            }
         }
     }
 }
