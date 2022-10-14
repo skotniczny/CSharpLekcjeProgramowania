ea3f72b1676fe0607150e9ee7c952e8324ba4ba0
Łukasz <skotniczny@outlook.com>
2022-10-14 20:05:07 +0200

    Dni tygodnia

---
+++ Z0703/Program.cs
@@ -0,0 +1,16 @@
+﻿Random r = new Random();
+int n = r.Next(8); // losowanie liczby od 0 do 7
+string[] dniTygodnia =
+    {
+        "",
+        "niedziela",
+        "poniedziałek",
+        "wtorek",
+        "środa",
+        "czwartek",
+        "piątek",
+        "sobota",
+    };
+string opis = dniTygodnia[n];
+
+Console.WriteLine("Dzień tygodnia: " + n + ", " + opis);
