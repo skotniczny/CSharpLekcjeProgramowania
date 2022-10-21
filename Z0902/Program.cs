fb8df5f5c0855cfe77884aafdc39e0a7981f0063
Łukasz <skotniczny@outlook.com>
2022-10-22 00:18:45 +0200

    Szyfr Cezara

---
+++ Z0902/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+static string CesarEncode(string text, int mov)
+{
+    Dictionary<char, char> dict = new Dictionary<char, char>();
+
+    for (char c = 'A'; c <= 'Z'; c++)
+    {
+        int pos = c + mov;
+        dict[c] = (char)((pos <= 'Z') ? pos : (pos % 'Z') + '@');
+    }
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        int pos = c + mov;
+        dict[c] = (char)((pos <= 'z') ? pos : (pos % 'z') + '`');
+    }
+
+    StringBuilder output = new StringBuilder();
+    foreach (char c in text)
+    {
+        output.Append(dict.ContainsKey(c) ? dict[c] : c);
+    }
+
+    return output.ToString();
+}
+
+Console.WriteLine(CesarEncode("abcABC xyzXYZ", 1));
+Console.WriteLine(CesarEncode("bababadalgharaghtakamminarronnkonnbronntonnerronntuonnthunntrovarrhounawnskawntoohoohoordenenthurnuk!", 26));
