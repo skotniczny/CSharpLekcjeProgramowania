d7f210f68c5fff55345d172178cf145d3bf9b171
Łukasz <skotniczny@outlook.com>
2022-10-22 09:21:56 +0200

    Usuwanie duplikatów

---
+++ Z0903/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] Uniq(double[] array)
+{
+    List<double> uniq = new List<double>();
+
+    foreach (double element in array)
+    {
+        if (!uniq.Contains(element)) uniq.Add(element);
+    }
+
+    return uniq.ToArray();
+}
+
+Console.WriteLine(string.Join("; ", Uniq(new double[] { 1.111, 1.111, 1.4, 1.4, 1.8, 2.4, 2.4, 100.5, 1.111, 1.4})));
