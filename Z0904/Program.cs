8b331dd21e71c47ed9683cb851f0ae3b3bcbbc2b
Łukasz <skotniczny@outlook.com>
2022-10-22 13:49:15 +0200

    Modyfikacja metody `przepiszElementyDoTablicy`

---
+++ Z0904/Program.cs
@@ -0,0 +1,15 @@
+﻿static T[] przepiszElementyDoTablicy<T>(IEnumerable<T> lista)
+{
+    List<T> l = new List<T>();
+    foreach (T element in lista) l.Add(element);
+
+    return l.ToArray();
+}
+
+Stack<string> s = new Stack<string>();
+s.Push("a");
+s.Push("b");
+s.Push("c");
+
+string[] tablica = przepiszElementyDoTablicy(s);
+Console.WriteLine(string.Join(',', tablica));
