062aaa8dbea9fee0c5de9ad9f3d92bc7c0c520b4
Łukasz <skotniczny@outlook.com>
2022-11-06 17:55:56 +0100

    Singleton. Rejestrowanie zdarzeń w aplikacji

---
+++ RejestrowanieNieprzewidzianychZdarzeń/Log.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RejestrowanieNieprzewidzianychZdarzeń
+{
+    public class Log
+    {
+        #region Singleton
+        private static Log instancja;
+
+        private Log()
+        {
+            Resetuj();
+        }
+
+        public static Log Instancja
+        {
+            get
+            {
+                if (instancja == null) instancja = new Log();
+                return instancja;
+            }
+        }
+        #endregion
+
+        #region Rejestrowanie zdarzeń
+        private List<string> rejestr = null;
+        private DateTime czasRozpoczęciaRejestracji;
+
+        public void Resetuj()
+        {
+            czasRozpoczęciaRejestracji = DateTime.Now;
+            rejestr = new List<string>();
+            rejestr.Add("#Data rozpoczęcia rejestracji: " + DateTime.Now.ToString());
+            rejestr.Add("#Nazwa komputera: " + Environment.MachineName);
+        }
+
+        public void Dodaj(string komunikat)
+        {
+            double liczaMilisekundOdUruchomienia = (DateTime.Now - czasRozpoczęciaRejestracji).TotalMilliseconds;
+            rejestr.Add(liczaMilisekundOdUruchomienia.ToString(System.Globalization.CultureInfo.InvariantCulture) + ": " + komunikat);
+        }
+
+        public void Zapisz()
+        {
+            rejestr.Add("#Data zakończenia rejestracji: " + DateTime.Now.ToString());
+            System.IO.File.AppendAllLines("Log.txt", rejestr);
+            Resetuj();
+        }
+        #endregion
+    }
+}
---
+++ RejestrowanieNieprzewidzianychZdarzeń/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace RejestrowanieNieprzewidzianychZdarzeń
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Action akcja = async () =>
+            {
+                int licznik = 0;
+                while (true)
+                {
+                    Log.Instancja.Dodaj("Licznik: " + licznik.ToString());
+                    licznik++;
+                    await Task.Delay(1000);
+                }
+            };
+            Task.Run(akcja);
+
+            //2
+            Console.WriteLine("Wciskaj klawisze. Koniec - naciścięcie Escape");
+            ConsoleKeyInfo cki;
+            do
+            {
+                cki = Console.ReadKey(false);
+                Log.Instancja.Dodaj("Naciśnięty klawisz: " + cki.Key.ToString());
+            }
+            while (cki.Key != ConsoleKey.Escape);
+            Log.Instancja.Zapisz();
+        }
+    }
+}
