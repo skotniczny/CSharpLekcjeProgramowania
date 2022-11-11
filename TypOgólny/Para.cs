a427d72faa5d277539a0edb67ae0933d116b0169
Łukasz <skotniczny@outlook.com>
2022-11-11 17:54:42 +0100

    Typy ogólne

---
+++ TypOgólny/Para.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypOgólny2
+{
+    public class Para<T1,T2> : IComparable<Para<T1,T2>>
+        where T1 : IComparable<T1>
+        where T2 : IComparable<T2>
+    {
+        private T1 pierwszy = default(T1);
+        private T2 drugi = default(T2);
+        public Para(T1 pierwszy, T2 drugi)
+        {
+            this.pierwszy = pierwszy;
+            this.drugi = drugi;
+        }
+        public override string ToString()
+        {
+            return pierwszy.ToString() + "\t" + drugi.ToString();
+        }
+
+        public int CompareTo(Para<T1,T2> innaPara)
+        {
+            int wartosc = this.pierwszy.CompareTo(innaPara.pierwszy);
+            if (wartosc != 0) return wartosc;
+            return this.drugi.CompareTo(innaPara.drugi);
+        }
+    }
+}
---
+++ TypOgólny/Program.cs
@@ -0,0 +1,71 @@
+﻿using ParaInt = TypOgólny2.Para<int, int>;
+using ParaDouble = TypOgólny2.Para<double, double>;
+using ParaString = TypOgólny2.Para<string, string>;
+using ParaIntDouble = TypOgólny2.Para<int, double>;
+
+namespace TypOgólny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            //int
+            ParaInt[] pi = new ParaInt[10];
+            for (int i = 0; i < pi.Length; i++)
+            {
+                pi[i] = new ParaInt(r.Next(10), r.Next(10));
+            }
+            Console.WriteLine("ParaInt:");
+            foreach (ParaInt para in pi) Console.WriteLine(para.ToString());
+            Array.Sort(pi);
+            Console.WriteLine("\nParaInt po sortowaniu:");
+            foreach (ParaInt para in pi) Console.WriteLine(para.ToString());
+
+            //double
+            ParaDouble[] pd = new ParaDouble[10];
+            for (int i = 0; i < pi.Length; i++)
+            {
+                pd[i] = new ParaDouble(r.NextDouble(), r.NextDouble());
+            }
+            Console.WriteLine("\nParaDouble:");
+            foreach (ParaDouble para in pd) Console.WriteLine(para.ToString());
+            Array.Sort(pd);
+            Console.WriteLine("\nParaDouble po sortowaniu");
+            foreach (ParaDouble para in pd) Console.WriteLine(para.ToString());
+
+            //int-double
+            ParaIntDouble[] p = new ParaIntDouble[10];
+            for (int i = 0; i < pi.Length; i++)
+            {
+                p[i] = new ParaIntDouble(r.Next(10), r.NextDouble());
+            }
+            Console.WriteLine("\nPara<int, double>");
+            foreach (ParaIntDouble para in p) Console.WriteLine(para.ToString());
+            Array.Sort(p);
+            Console.WriteLine("\nPara<int, double> po sortowaniu");
+            foreach (ParaIntDouble para in p) Console.WriteLine(para.ToString());
+
+            //string
+            ParaString[] ps = new ParaString[12];
+            ps[0] = new ParaString("Bester", "Alfred");
+            ps[1] = new ParaString("Dick", "Philip");
+            ps[2] = new ParaString("Tolkien", "John");
+            ps[3] = new ParaString("Lem", "Stanisław");
+            ps[4] = new ParaString("Anderson", "Poul");
+            ps[5] = new ParaString("Pohl", "Frederik");
+            ps[6] = new ParaString("Le Guin", "Ursula");
+            ps[7] = new ParaString("Card", "Orson");
+            ps[8] = new ParaString("Gibson", "William");
+            ps[9] = new ParaString("Asimov", "Isaac");
+            ps[10] = new ParaString("Niven", "Larry");
+            ps[11] = new ParaString("Herbert", "Frank");
+            Console.WriteLine("\nParaString:");
+            foreach (ParaString para in ps) Console.WriteLine(para.ToString());
+            Array.Sort(ps);
+            Console.WriteLine("\nParaString po sortowaniu:");
+            foreach (ParaString para in ps) Console.WriteLine(para.ToString());
+        }
+    }
+}
