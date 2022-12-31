377d065d2e1438972f5d8bb163e6b4b97079e30e
Łukasz <skotniczny@outlook.com>
2022-12-31 10:43:30 +0100

    Pracownik (liść)

---
+++ Kadry/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kadry
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(new Pracownik("Jan", "Kowalski", "asystent", 2517.4M).ToString());
+        }
+    }
+}
---
+++ Kadry/StrukturaPracowników.cs
@@ -0,0 +1,23 @@
+﻿namespace Kadry
+{
+    public class Pracownik
+    {
+        private string imię;
+        private string nazwisko;
+        private string stanowisko;
+        private decimal pensja;
+
+        public Pracownik(string imię, string nazwisko, string stanowisko, decimal pensja)
+        {
+            this.imię = imię;
+            this.nazwisko = nazwisko;
+            this.stanowisko = stanowisko;
+            this.pensja = pensja;
+        }
+
+        public override string ToString()
+        {
+            return $"{imię} {nazwisko}, {stanowisko} ({pensja:C})";
+        }
+    }
+}
