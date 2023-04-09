ad6c10cd46232bd1247aa52752ee848a09cbd665
Łukasz <skotniczny@outlook.com>
2023-04-09 23:21:52 +0200

    Zdefiniowanie właściwości tylko do odczytu Długość

---
+++ Z1102/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Z1102
+{
+    internal class Program
+    {
+        public struct Wektor
+        {
+            public Wektor(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public double X;
+            public double Y;
+            public double Z;
+
+            public double Długość
+            {
+                get
+                {
+                    return Math.Sqrt(X * X + Y * Y + Z * Z);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Wektor wektor1 = new Wektor(1, 2, 3);
+            Console.WriteLine(wektor1.Długość);
+        }
+    }
+}
