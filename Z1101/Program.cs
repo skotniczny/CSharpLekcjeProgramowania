9e480f76e2a17d0874fc631869a0e8008aa4d29b
Łukasz <skotniczny@outlook.com>
2023-04-09 22:56:32 +0200

    Zdefiniowanie publicznej struktury Wektor

---
+++ Z1101/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Z1101
+{
+    internal class Program
+    {
+        public struct Wektor {
+            public Wektor(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public double X;
+            public double Y;
+            public double Z;
+        }
+        static void Main(string[] args)
+        {
+            Wektor wektor1= new Wektor(1, 2, 3);
+        }
+    }
+}
