e194a6716a8777824bea321e2010fb18016be99d
Łukasz <skotniczny@outlook.com>
2023-05-25 00:37:29 +0200

    Widok

---
+++ Konsola/Widok/StosowanieUstawieńKonsoli.cs
@@ -0,0 +1,29 @@
+﻿using Konsola.Model;
+
+namespace Konsola.Widok
+{
+    public class StosowanieUstawieńKonsoli
+    {
+        public static bool ZastosujUstawienia(UstawieniaKonsoli ustawienia, bool wyczyśćKonsolę = true)
+        {
+            try
+            {
+                Console.BackgroundColor = ustawienia.KolorTła;
+                Console.ForegroundColor = ustawienia.KolorCzcionki;
+                Console.SetWindowSize(ustawienia.RozmiarOkna.Szerokość, ustawienia.RozmiarOkna.Wysokość);
+                Console.SetBufferSize(ustawienia.RozmiarBufora.Szerokość, ustawienia.RozmiarBufora.Wysokość);
+                Console.Title = ustawienia.Tytuł;
+                if (wyczyśćKonsolę)
+                {
+                    Console.Clear();
+                }
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.Error.WriteLine($"Wystąpił błąd: {exc.Message}");
+                return false;
+            }
+        }
+    }
+}
