d164d4bc18e1531171e615dd7daf2ecfbd5fdf06
Łukasz <skotniczny@outlook.com>
2023-05-25 00:26:10 +0200

    Model

---
+++ Konsola/Model/UstawieniaKonsoli.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Konsola.Model
+{
+    public struct Rozmiar
+    {
+        public int Szerokość;
+        public int Wysokość;
+
+        public override string ToString()
+        {
+            return $" {Szerokość} x {Wysokość}";
+        }
+    }
+    public class UstawieniaKonsoli
+    {
+        public ConsoleColor KolorTła;
+        public ConsoleColor KolorCzcionki;
+        public Rozmiar RozmiarOkna;
+        public Rozmiar RozmiarBufora;
+        public string Tytuł;
+
+        public override string ToString()
+        {
+            return $"Kolor tła: {KolorTła.ToString()}" +
+                $"\nKolor czcionki: {KolorCzcionki.ToString()}" +
+                $"\nRozmiar okna: {RozmiarOkna.ToString()}" +
+                $"\nRozmiar bufora: {RozmiarBufora.ToString()}" +
+                $"\nTytuł: {Tytuł}";
+        }
+    }
+}
---
+++ Konsola/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Konsola
+{
+    using Model;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UstawieniaKonsoli model = PomocnikUstawieńKonsoli.UstawieniaBieżące;
+            Console.WriteLine(model.ToString());
+        }
+    }
+}
