8af8f7664d2fd5841e9a38960c60773fbd150f04
Łukasz <skotniczny@outlook.com>
2023-05-25 00:26:10 +0200

    Model

---
+++ Konsola/Model/PomocnikUstawieńKonsoli.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Konsola.Model
+{
+    public static class PomocnikUstawieńKonsoli //helper
+    {
+        public static UstawieniaKonsoli UstawieniaBieżące
+        {
+            get
+            {
+                return new UstawieniaKonsoli()
+                {
+                    KolorTła = Console.BackgroundColor,
+                    KolorCzcionki = Console.ForegroundColor,
+                    RozmiarOkna =
+                    {
+                        Szerokość = Console.WindowWidth,
+                        Wysokość = Console.WindowHeight
+                    },
+                    RozmiarBufora =
+                    {
+                        Szerokość = Console.BufferWidth,
+                        Wysokość = Console.BufferHeight
+                    },
+                    Tytuł = Console.Title
+                };
+            }
+        }
+
+        private static UstawieniaKonsoli ustawieniaDomyślne = new UstawieniaKonsoli()
+        {
+            KolorTła = ConsoleColor.Black,
+            KolorCzcionki = ConsoleColor.Gray,
+            RozmiarOkna = { Szerokość = 120, Wysokość = 30 },
+            RozmiarBufora = { Szerokość = 120, Wysokość = 9001 },
+            Tytuł = System.Reflection.Assembly.GetEntryAssembly().Location
+        };
+
+        public static UstawieniaKonsoli UstawieniaDomyślne
+        {
+            get => ustawieniaDomyślne;
+        }
+    }
+}
