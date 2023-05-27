53be6970036cf9ef433439282505fd196a09a4dc
Łukasz <skotniczny@outlook.com>
2023-05-27 22:37:13 +0200

    Tworzenie pliku XML za pomocą klas XDocument i XElement

--- Konsola/Program.cs
+++ Konsola/Program.cs
@@ -5,12 +5,17 @@
 
     class Program
     {
+        private const string ścieżkaPliku = "ustawienia.xml";
+
         static void Main(string[] args)
         {
             UstawieniaKonsoli model = PomocnikUstawieńKonsoli.UstawieniaBieżące;
 
             Menu kontroler = new Menu(model);
             kontroler.Uruchom();
+
+            model.Zapisz(ścieżkaPliku);
+            Console.WriteLine($"Ustawiwania zapisane do pliku {ścieżkaPliku}");
         }
     }
 }
