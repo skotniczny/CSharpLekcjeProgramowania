294909ec1dddf4ff51f89077d2de5c11356c61be
Łukasz <skotniczny@outlook.com>
2023-05-28 13:09:11 +0200

    Drzewo i rekurencja

---
+++ Drzewo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+
+namespace Drzewo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xml = XDocument.Load("ustawienia.xml");
+            Węzeł drzewo = xml.TwórzDrzewo();
+        }
+    }
+}
---
+++ Drzewo/Węzeł.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Drzewo
+{
+    public class Węzeł
+    {
+        public string Nazwa = null;
+        public string Wartość = null;
+        public List<Węzeł> Węzły = new List<Węzeł>();
+
+        public override string ToString()
+        {
+            return $"{Nazwa} {((Wartość != null) ? ($": {Wartość}") : "")}";
+        }
+    }
+    public static class DrzewoXml
+    {
+        private static void dodajWęzeł(XElement elementXml, Węzeł węzeł)
+        {
+            IEnumerable<XElement> elementy = elementXml.Elements();
+            foreach (XElement element in elementy)
+            {
+                string nazwaWęzła = element.Name.LocalName;
+                string wartośćWęzła = null;
+                if (!element.HasElements && !string.IsNullOrEmpty(element.Value))
+                {
+                    wartośćWęzła += element.Value;
+                }
+                Węzeł nowyWęzeł = new Węzeł()
+                {
+                    Nazwa = nazwaWęzła,
+                    Wartość = wartośćWęzła,
+                };
+                węzeł.Węzły.Add(nowyWęzeł);
+                dodajWęzeł(element, nowyWęzeł);
+            }
+        }
+
+        public static Węzeł TwórzDrzewo(this XDocument xml)
+        {
+            Węzeł drzewo = new Węzeł()
+            {
+                Nazwa = xml.Root.Name.LocalName,
+            };
+            dodajWęzeł(xml.Root, drzewo);
+            return drzewo;
+        }
+    }
+}
