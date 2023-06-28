591d55880d042824809988d6378f49478b0b894b
Łukasz <skotniczny@outlook.com>
2023-06-28 23:08:59 +0200

    Baza danych i tabele

---
+++ EF/BazaDanych/BazaDanych.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF.BazaDanych
+{
+    class BazaDanychOsóbDbContext : DbContext
+    {
+        public DbSet<Osoba> Osoby { get; set; }
+        public DbSet<Adres> Adresy { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=osoby.db");
+        }
+    }
+
+    public class BazaDanychOsób : IDisposable
+    {
+        private BazaDanychOsóbDbContext dbc = new BazaDanychOsóbDbContext();
+
+        public BazaDanychOsób()
+        {
+            dbc.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            dbc.Dispose();
+        }
+
+        #region Debugowanie
+#if DEBUG
+        public Osoba[] Osoby => dbc.Osoby.ToArray();
+        public Adres[] Adresy => dbc.Adresy.ToArray();
+#endif
+        #endregion
+
+        public Osoba PobierzOsobę(int idOsoby)
+        {
+            return dbc.Osoby.FirstOrDefault(o => o.Id == idOsoby);
+        }
+
+        public Osoba this[int idOsoby] => PobierzOsobę(idOsoby);
+
+        public int[] IdentyfikatoryOsób => dbc.Osoby.Select(o => o.Id).ToArray();
+    }
+}
