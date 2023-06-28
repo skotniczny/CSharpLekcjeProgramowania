8b36a2d4ad50d40465a84bd137d648b06ee66af1
Łukasz <skotniczny@outlook.com>
2023-06-28 23:14:25 +0200

    Dodawanie rekordów do tabeli

--- EF/BazaDanych/BazaDanych.cs
+++ EF/BazaDanych/BazaDanych.cs
@@ -42,5 +42,33 @@ namespace EF.BazaDanych
         public Osoba this[int idOsoby] => PobierzOsobę(idOsoby);
 
         public int[] IdentyfikatoryOsób => dbc.Osoby.Select(o => o.Id).ToArray();
+
+        public int DodajOsobę(Osoba osoba)
+        {
+            if (osoba == null) throw new ArgumentNullException("Podano pustą referencję jako argument");
+            if (osoba.Adres == null) throw new ArgumentException("Brak adresu zameldowania");
+
+            if (dbc.Osoby.Any(o =>  o.Id == osoba.Id))
+            {
+                osoba.Id = dbc.Osoby.Max(o => o.Id) + 1;
+            }
+            else
+            {
+                if (dbc.Osoby.Any(o => o.Id == osoba.Id))
+                {
+                    osoba.Id = dbc.Osoby.Max(o => o.Id) + 1;
+                }
+            }
+
+            // unikanie dublowania adresów
+            Adres adres = dbc.Adresy.ToArray().FirstOrDefault(a => a.Equals(osoba.Adres));
+            if (adres != null) osoba.Adres = adres; // adres już jest, biorę z bazy danych
+
+            // zmiany w bazie
+            dbc.Osoby.Add(osoba);
+            dbc.SaveChanges();
+
+            return osoba.Id;
+        }
     }
 }
