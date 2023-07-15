dd1a2313233033f444f7f938084dd61e7646e357
Łukasz <skotniczny@outlook.com>
2023-07-15 12:11:11 +0200

    Katalogi specjalne zdefiniowane w bieżącym profilu użytkownika

--- InformacjeOSystemie/Program.cs
+++ InformacjeOSystemie/Program.cs
@@ -8,9 +8,20 @@
             $"\nNazwa komputera: {Environment.MachineName}" +
             $"\nKatalog systemowy: {Environment.SystemDirectory}";
 
+        static string użytkownik = $"Nazwa użytkownika: {Environment.UserName}" +
+            "\nKatalogi specjalne użytkownika:" +
+            $"\nkatalog 'Moje dokumenty' = {Environment.GetFolderPath(Environment.SpecialFolder.Personal)}" +
+            $"\nkatalog 'Pulpit' = {Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}" +
+            $"\nkatalog 'Moje obrazy' = {Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)}" +
+            $"\nkatalog menu Start = {Environment.GetFolderPath(Environment.SpecialFolder.StartMenu)}" +
+            $"\nkatalog 'Programy' = {Environment.GetFolderPath(Environment.SpecialFolder.Programs)}" +
+            $"\nkatalog 'Autostart' = {Environment.GetFolderPath(Environment.SpecialFolder.Startup)}" +
+            $"\nkatalog domowy użytkownika = {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
+
         static void Main(string[] args)
         {
             Console.WriteLine($"Informacje o systemie:\n{system}");
+            Console.WriteLine($"\nInformacje o użytkowniku:\n{użytkownik}");
         }
     }
 }
