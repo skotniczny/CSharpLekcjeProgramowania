50324671259e93a74c2eeb5cb0e5badde7a64b83
Łukasz <skotniczny@outlook.com>
2023-07-15 09:46:45 +0200

    Podstawowe informacje o systemie i profilu użytkownika

---
+++ InformacjeOSystemie/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InformacjeOSystemie
+{
+    internal class Program
+    {
+        static string system = $"Wersja systemu: {Environment.OSVersion} " +
+            (Environment.Is64BitOperatingSystem ? ", 64 bitowy" : "") +
+            $"\nWersja Microsoft .NET Framework: {Environment.Version}" +
+            $"\nNazwa komputera: {Environment.MachineName}" +
+            $"\nKatalog systemowy: {Environment.SystemDirectory}";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Informacje o systemie:\n{system}");
+        }
+    }
+}
